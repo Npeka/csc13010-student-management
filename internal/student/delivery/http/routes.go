0f8f20b558142ae95444fc7a49219ff1084961e7
Npeka <npkhang22@clc.fitus.edu.vn>
2025-02-25 19:39:39 +0700

    feat: router map student handlers

---
+++ internal/student/delivery/http/routes.go
@@ -0,0 +1,14 @@
+package http
+
+import (
+	"github.com/csc13010-student-management/internal/student"
+	"github.com/gin-gonic/gin"
+)
+
+func MapStudentHandlers(stGroup *gin.RouterGroup, h student.IStudentHandler) {
+	stGroup.GET("", h.GetStudents())
+	stGroup.POST("", h.CreateStudent())
+	stGroup.PATCH("/:student_id", h.UpdateStudent())
+	stGroup.DELETE("/:student_id", h.DeleteStudent())
+	stGroup.GET("/search", h.SearchStudents())
+}

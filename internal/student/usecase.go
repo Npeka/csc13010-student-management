e54d40ac508adb25763d7d4d7aba94379b933a3d
Npeka <npkhang22@clc.fitus.edu.vn>
2025-02-27 06:24:57 +0700

    feat: update student usecase

--- internal/student/usecase.go
+++ internal/student/usecase.go
@@ -13,7 +13,21 @@ type IStudentUsecase interface {
 	CreateStudent(ctx context.Context, student *models.Student) error
 	UpdateStudent(ctx context.Context, student *models.Student) error
 	DeleteStudent(ctx context.Context, student_id string) error
-	GetOptions(ctx context.Context) (*dtos.OptionDTO, error)
+
 	ImportStudents(ctx context.Context, filePath string) error
 	ExportStudents(ctx context.Context, format string) (string, error)
+
+	GetOptions(ctx context.Context) (*dtos.OptionDTO, error)
+
+	GetFaculties(ctx context.Context) ([]*models.Faculty, error)
+	GetPrograms(ctx context.Context) ([]*models.Program, error)
+	GetStatuses(ctx context.Context) ([]*models.Status, error)
+
+	CreateFaculty(ctx context.Context, faculty *models.Faculty) error
+	CreateProgram(ctx context.Context, program *models.Program) error
+	CreateStatus(ctx context.Context, status *models.Status) error
+
+	DeleteFaculty(ctx context.Context, faculty_id int) error
+	DeleteProgram(ctx context.Context, program_id int) error
+	DeleteStatus(ctx context.Context, status_id int) error
 }

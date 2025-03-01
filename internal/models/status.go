cb7d3c7d91f74148adfd344a7ca120fbccdb090c
Npeka <npkhang22@clc.fitus.edu.vn>
2025-03-01 20:57:43 +0700

    refactor: update status model

--- internal/models/status.go
+++ internal/models/status.go
@@ -1,10 +1,21 @@
 package models
 
 type Status struct {
-	ID   int    `gorm:"primaryKey;autoIncrement;" json:"id"`
+	ID   uint   `gorm:"primaryKey;autoIncrement;" json:"id"`
 	Name string `gorm:"type:varchar(255);uniqueIndex;not null" json:"name"`
 }
 
 func (Status) TableName() string {
 	return "statuses"
+}
+
+type StatusTransition struct {
+	ID              uint   `gorm:"primaryKey"`
+	CurrentStatusID uint   `gorm:"not null"`
+	NewStatusID     uint   `gorm:"not null"`
+	AllowedRoles    string `gorm:"type:varchar(255);not null"`
+}
+
+func (StatusTransition) TableName() string {
+	return "status_transitions"
 }

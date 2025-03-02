f4f9d171b7fe9a2595ebb308c9542a771becdd3d
Npeka <npkhang22@clc.fitus.edu.vn>
2025-03-02 12:15:21 +0700

    feat: add opentracing and error handler to auditlog repo

--- internal/auditlog/repository/repository.go
+++ internal/auditlog/repository/repository.go
@@ -5,6 +5,8 @@ import (
 
 	"github.com/csc13010-student-management/internal/auditlog"
 	"github.com/csc13010-student-management/internal/models"
+	"github.com/opentracing/opentracing-go"
+	"github.com/pkg/errors"
 	"gorm.io/gorm"
 )
 
@@ -18,18 +20,26 @@ func NewAuditLogRepository(db *gorm.DB) auditlog.IAuditLogRepository {
 
 // GetAuditLogs implements auditlog.IAuditLogRepository.
 func (a *auditLogRepository) GetAuditLogs(ctx context.Context) ([]*models.AuditLog, error) {
+	span, ctx := opentracing.StartSpanFromContext(ctx, "auditlog.GetAuditLogs")
+	defer span.Finish()
+
 	var auditLogs []*models.AuditLog
-	if err := a.db.Find(&auditLogs).Error; err != nil {
-		return nil, err
+	if err := a.db.WithContext(ctx).Find(&auditLogs).Error; err != nil {
+		return nil, errors.Wrap(err, "auditlog.GetAuditLogs.Find")
 	}
+
 	return auditLogs, nil
 }
 
 // GetModelAuditLogs implements auditlog.IAuditLogRepository.
 func (a *auditLogRepository) GetModelAuditLogs(ctx context.Context, table_name string, record_id string) ([]*models.AuditLog, error) {
+	span, ctx := opentracing.StartSpanFromContext(ctx, "auditlog.GetModelAuditLogs")
+	defer span.Finish()
+
 	var auditLogs []*models.AuditLog
-	if err := a.db.Where("table_name = ? AND record_id = ?", table_name, record_id).Find(&auditLogs).Error; err != nil {
-		return nil, err
+	if err := a.db.WithContext(ctx).Where("table_name = ? AND record_id = ?", table_name, record_id).Find(&auditLogs).Error; err != nil {
+		return nil, errors.Wrap(err, "auditlog.GetModelAuditLogs.Find")
 	}
+
 	return auditLogs, nil
 }

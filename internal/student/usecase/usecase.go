3f8ea711fe213d0707b891d5b3a3e71b8af87482
Npeka <npkhang22@clc.fitus.edu.vn>
2025-03-02 17:09:56 +0700

    refactor: update student usecase

--- internal/student/usecase/usecase.go
+++ internal/student/usecase/usecase.go
@@ -2,89 +2,134 @@ package usecase
 
 import (
 	"context"
+	"encoding/json"
 
+	"github.com/casbin/casbin/v2"
+	"github.com/csc13010-student-management/internal/events"
 	"github.com/csc13010-student-management/internal/models"
 	"github.com/csc13010-student-management/internal/student"
 	"github.com/csc13010-student-management/internal/student/dtos"
+	"github.com/opentracing/opentracing-go"
+	"github.com/pkg/errors"
+
 	"github.com/csc13010-student-management/pkg/logger"
-	"go.uber.org/zap"
+	"github.com/segmentio/kafka-go"
 )
 
 type studentUsecase struct {
 	sr student.IStudentRepository
 	lg *logger.LoggerZap
+	kw *kafka.Writer
+	e  *casbin.Enforcer
 }
 
 func NewStudentUsecase(
 	sr student.IStudentRepository,
 	lg *logger.LoggerZap,
+	kw *kafka.Writer,
+	e *casbin.Enforcer,
 ) student.IStudentUsecase {
 	return &studentUsecase{
 		sr: sr,
 		lg: lg,
+		kw: kw,
+		e:  e,
 	}
 }
 
-func (s *studentUsecase) logAndReturnError(msg string, err error) error {
-	s.lg.Error(msg, zap.Error(err))
-	return err
-}
+func (su *studentUsecase) GetStudents(ctx context.Context) (students []*models.Student, err error) {
+	span, ctx := opentracing.StartSpanFromContext(ctx, "studentUsecase.GetStudents")
+	defer span.Finish()
 
-func (s *studentUsecase) GetStudents(ctx context.Context) (students []*models.Student, err error) {
-	students, err = s.sr.GetStudents(ctx)
+	students, err = su.sr.GetStudents(ctx)
 	if err != nil {
-		return nil, s.logAndReturnError("Failed to get students", err)
+		return nil, err
 	}
-	s.lg.Info("Successfully fetched students")
 	return students, nil
 }
 
-func (s *studentUsecase) GetStudentByStudentID(ctx context.Context, studentID string) (student *models.Student, err error) {
-	student, err = s.sr.GetStudentByStudentID(ctx, studentID)
-	if err != nil {
-		return nil, s.logAndReturnError("Failed to get student", err)
-	}
-	s.lg.Info("Successfully fetched student", zap.String("id", studentID))
-	return student, nil
-}
+func (su *studentUsecase) GetStudentByStudentID(ctx context.Context, studentID string) (student *models.Student, err error) {
+	span, ctx := opentracing.StartSpanFromContext(ctx, "studentUsecase.GetStudentByStudentID")
+	defer span.Finish()
 
-func (s *studentUsecase) GetFullInfoStudentByStudentID(ctx context.Context, studentID string) (student *dtos.StudentDTO, err error) {
-	student, err = s.sr.GetFullInfoStudentByStudentID(ctx, studentID)
+	student, err = su.sr.GetStudentByStudentID(ctx, studentID)
 	if err != nil {
-		return nil, s.logAndReturnError("Failed to get full info student", err)
+		return nil, err
 	}
 	return student, nil
 }
 
-func (s *studentUsecase) CreateStudent(ctx context.Context, student *models.Student) error {
-	if err := s.sr.CreateStudent(ctx, student); err != nil {
-		return s.logAndReturnError("Failed to create student", err)
-	}
-	s.lg.Info("Successfully created student", zap.Int("id", student.ID))
-	return nil
-}
+func (su *studentUsecase) GetFullInfoStudentByStudentID(ctx context.Context, studentID string) (student *dtos.StudentDTO, err error) {
+	span, ctx := opentracing.StartSpanFromContext(ctx, "studentUsecase.GetFullInfoStudentByStudentID")
+	defer span.Finish()
 
-func (s *studentUsecase) UpdateStudent(ctx context.Context, student *models.Student) error {
-	if err := s.sr.UpdateStudent(ctx, student); err != nil {
-		return s.logAndReturnError("Failed to update student", err)
-	}
-	s.lg.Info("Successfully updated student", zap.Int("id", student.ID))
-	return nil
-}
-
-func (s *studentUsecase) DeleteStudent(ctx context.Context, studentID string) error {
-	if err := s.sr.DeleteStudent(ctx, studentID); err != nil {
-		return s.logAndReturnError("Failed to delete student", err)
-	}
-	s.lg.Info("Successfully deleted student", zap.String("id", studentID))
-	return nil
-}
-
-func (s *studentUsecase) GetOptions(ctx context.Context) (options *dtos.OptionDTO, err error) {
-	options, err = s.sr.GetOptions(ctx)
+	student, err = su.sr.GetFullInfoStudentByStudentID(ctx, studentID)
 	if err != nil {
-		return nil, s.logAndReturnError("Failed to get student options", err)
+		return nil, err
 	}
-	s.lg.Info("Successfully fetched student options")
+	return student, nil
+}
+
+func (su *studentUsecase) CreateStudent(ctx context.Context, student *models.Student) error {
+	span, ctx := opentracing.StartSpanFromContext(ctx, "studentUsecase.CreateStudent")
+	defer span.Finish()
+
+	// Create Student
+	if err := su.sr.CreateStudent(ctx, student); err != nil {
+		return err
+	}
+
+	// Publish Kafka event
+	studentJson, err := json.Marshal(events.StudentCreatedEvent{
+		Username: student.StudentID,
+		Role:     models.RoleStudent,
+	})
+	if err != nil {
+		return errors.Wrap(err, "studentUsecase.CreateStudent.json.Marshal")
+	}
+	if err := su.kw.WriteMessages(ctx, kafka.Message{Value: studentJson}); err != nil {
+		return errors.Wrap(err, "studentUsecase.CreateStudent.kw.WriteMessages")
+	}
+	return nil
+}
+
+func (su *studentUsecase) UpdateStudent(ctx context.Context, student *models.Student) error {
+	span, ctx := opentracing.StartSpanFromContext(ctx, "studentUsecase.UpdateStudent")
+	defer span.Finish()
+
+	if err := su.sr.UpdateStudent(ctx, student); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (su *studentUsecase) UpdateUserIDByUsername(ctx context.Context, studentID string, userID uint) error {
+	span, ctx := opentracing.StartSpanFromContext(ctx, "studentUsecase.UpdateUserIDByUsername")
+	defer span.Finish()
+
+	if err := su.sr.UpdateUserIDByUsername(ctx, studentID, userID); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (su *studentUsecase) DeleteStudent(ctx context.Context, studentID string) error {
+	span, ctx := opentracing.StartSpanFromContext(ctx, "studentUsecase.DeleteStudent")
+	defer span.Finish()
+
+	if err := su.sr.DeleteStudent(ctx, studentID); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (su *studentUsecase) GetOptions(ctx context.Context) (optionsu *dtos.OptionDTO, err error) {
+	span, ctx := opentracing.StartSpanFromContext(ctx, "studentUsecase.GetOptions")
+	defer span.Finish()
+
+	options, err := su.sr.GetOptions(ctx)
+	if err != nil {
+		return nil, err
+	}
 	return options, nil
 }

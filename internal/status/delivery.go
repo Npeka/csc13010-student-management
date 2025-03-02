250d12ab59830b76a9ef8af3613ce5fe7a1323c2
Npeka <npkhang22@clc.fitus.edu.vn>
2025-03-02 13:53:00 +0700

    feat: add opentracing to status handlers

--- internal/status/delivery.go
+++ internal/status/delivery.go
@@ -5,5 +5,6 @@ import "github.com/gin-gonic/gin"
 type IStatusHandlers interface {
 	GetStatuses() gin.HandlerFunc
 	CreateStatus() gin.HandlerFunc
+	UpdateStatus() gin.HandlerFunc
 	DeleteStatus() gin.HandlerFunc
 }
--- internal/status/delivery/http/handlers.go
+++ internal/status/delivery/http/handlers.go
@@ -7,8 +7,9 @@ import (
 	"github.com/csc13010-student-management/internal/models"
 	"github.com/csc13010-student-management/internal/status"
 	"github.com/csc13010-student-management/pkg/logger"
+	"github.com/csc13010-student-management/pkg/response"
 	"github.com/gin-gonic/gin"
-	"go.uber.org/zap"
+	"github.com/opentracing/opentracing-go"
 )
 
 type statusHandlers struct {
@@ -28,59 +29,94 @@ func NewStatusHandlers(
 
 func (sh *statusHandlers) GetStatuses() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		sh.lg.Info("GetFaculties called")
-		statuses, err := sh.su.GetStatuses(c)
+		span, ctx := opentracing.StartSpanFromContext(c.Request.Context(), "status.GetStatuses")
+		defer span.Finish()
+
+		statuses, err := sh.su.GetStatuses(ctx)
 		if err != nil {
-			sh.lg.Error("Error getting statuses", zap.Error(err))
-			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			logger.ErrResponseWithLog(c, sh.lg, err)
+			response.Error(c, http.StatusInternalServerError)
 			return
 		}
 
-		sh.lg.Info("GetStatuses successful")
-		c.JSON(http.StatusOK, statuses)
+		response.Success(c, response.ErrCodeSuccess, statuses)
 	}
 }
 
 func (sh *statusHandlers) CreateStatus() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		sh.lg.Info("GetFaculties called")
+		span, ctx := opentracing.StartSpanFromContext(c.Request.Context(), "status.CreateStatus")
+		defer span.Finish()
+
 		var status models.Status
 		if err := c.ShouldBindJSON(&status); err != nil {
-			sh.lg.Error("Error binding JSON", zap.Error(err))
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			logger.ErrResponseWithLog(c, sh.lg, err)
+			response.Error(c, http.StatusBadRequest)
 			return
 		}
 
-		err := sh.su.CreateStatus(c, &status)
+		err := sh.su.CreateStatus(ctx, &status)
 		if err != nil {
-			sh.lg.Error("Error creating status", zap.Error(err))
-			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			logger.ErrResponseWithLog(c, sh.lg, err)
+			response.Error(c, http.StatusInternalServerError)
 			return
 		}
 
-		sh.lg.Info("GetFaculties successful")
-		c.JSON(http.StatusOK, status)
+		response.Success(c, response.ErrCodeSuccess, status)
+	}
+}
+
+func (sh *statusHandlers) UpdateStatus() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		span, ctx := opentracing.StartSpanFromContext(c.Request.Context(), "status.UpdateStatus")
+		defer span.Finish()
+
+		sh.lg.Info("UpdateStatus called")
+		id, err := strconv.Atoi(c.Param("id"))
+		if err != nil {
+			logger.ErrResponseWithLog(c, sh.lg, err)
+			response.Error(c, http.StatusBadRequest)
+			return
+		}
+
+		var status models.Status
+		if err := c.ShouldBindJSON(&status); err != nil {
+			logger.ErrResponseWithLog(c, sh.lg, err)
+			response.Error(c, http.StatusBadRequest)
+			return
+		}
+
+		status.ID = uint(id)
+		err = sh.su.UpdateStatus(ctx, &status)
+		if err != nil {
+			logger.ErrResponseWithLog(c, sh.lg, err)
+			response.Error(c, http.StatusInternalServerError)
+			return
+		}
+
+		response.Success(c, response.ErrCodeSuccess, status)
 	}
 }
 
 func (sh *statusHandlers) DeleteStatus() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		sh.lg.Info("DeleteStatus called")
+		span, ctx := opentracing.StartSpanFromContext(c.Request.Context(), "status.DeleteStatus")
+		defer span.Finish()
+
 		id, err := strconv.Atoi(c.Param("id"))
 		if err != nil {
-			sh.lg.Error("Invalid status ID", zap.Error(err))
-			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid status ID"})
+			logger.ErrResponseWithLog(c, sh.lg, err)
+			response.Error(c, http.StatusBadRequest)
 			return
 		}
 
-		err = sh.su.DeleteStatus(c, uint(id))
+		err = sh.su.DeleteStatus(ctx, uint(id))
 		if err != nil {
-			sh.lg.Error("Error deleting status", zap.Error(err))
-			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			logger.ErrResponseWithLog(c, sh.lg, err)
+			response.Error(c, http.StatusInternalServerError)
 			return
 		}
 
-		sh.lg.Info("DeleteStatus successful")
-		c.JSON(http.StatusOK, gin.H{"message": "Status deleted successfully"})
+		response.Success(c, response.ErrCodeSuccess, nil)
 	}
 }

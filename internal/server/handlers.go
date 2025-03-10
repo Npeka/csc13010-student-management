d428b9bc13b8f14bb9380d84f0ff082cc5f63a20
Npeka <npkhang22@clc.fitus.edu.vn>
2025-03-10 22:32:40 +0700

    refactor: update server handlers

--- internal/server/handlers.go
+++ internal/server/handlers.go
@@ -6,18 +6,27 @@ import (
 	alHttp "github.com/csc13010-student-management/internal/auditlog/delivery/http"
 	alRepository "github.com/csc13010-student-management/internal/auditlog/repository"
 	alUsecase "github.com/csc13010-student-management/internal/auditlog/usecase"
+	alWorker "github.com/csc13010-student-management/internal/auditlog/worker"
+	"github.com/csc13010-student-management/internal/events"
+	"github.com/csc13010-student-management/internal/migration"
+	"github.com/segmentio/kafka-go"
+
 	authHttp "github.com/csc13010-student-management/internal/auth/delivery/http"
 	authRepository "github.com/csc13010-student-management/internal/auth/repository"
 	authUsecase "github.com/csc13010-student-management/internal/auth/usecase"
 	authWorker "github.com/csc13010-student-management/internal/auth/worker"
+	cfHttp "github.com/csc13010-student-management/internal/config/delivery/http"
+	cfRepository "github.com/csc13010-student-management/internal/config/repository"
+	cfUsecase "github.com/csc13010-student-management/internal/config/usecase"
 	ftHttp "github.com/csc13010-student-management/internal/faculty/delivery/http"
 	ftRepository "github.com/csc13010-student-management/internal/faculty/repository"
 	ftUsecase "github.com/csc13010-student-management/internal/faculty/usecase"
 	fpHttp "github.com/csc13010-student-management/internal/fileprocessor/delivery/http"
 	fpRepository "github.com/csc13010-student-management/internal/fileprocessor/repository"
 	fpUsecase "github.com/csc13010-student-management/internal/fileprocessor/usecase"
-	"github.com/csc13010-student-management/internal/initialize"
 	"github.com/csc13010-student-management/internal/middleware"
+	notiRepository "github.com/csc13010-student-management/internal/notification/repository"
+	notiWorker "github.com/csc13010-student-management/internal/notification/worker"
 	pgHttp "github.com/csc13010-student-management/internal/program/delivery/http"
 	pgRepository "github.com/csc13010-student-management/internal/program/repository"
 	pgUsecase "github.com/csc13010-student-management/internal/program/usecase"
@@ -29,15 +38,17 @@ import (
 	stdRepository "github.com/csc13010-student-management/internal/student/repository"
 	stdUsecase "github.com/csc13010-student-management/internal/student/usecase"
 	stdWorker "github.com/csc13010-student-management/internal/student/worker"
-	kafkaUtils "github.com/csc13010-student-management/pkg/kafka"
+	kafkas "github.com/csc13010-student-management/pkg/kafka"
 )
 
 func (s *Server) MapHandlers() error {
-
 	// kafka
-	kurl := fmt.Sprintf("%s:%d", s.cfg.Kafka.Host, s.cfg.Kafka.Port)
-	kwUserCreated := kafkaUtils.NewKafkaWriter(kurl, initialize.KafkaAuthUserCreated)
-	kwStudentCreated := kafkaUtils.NewKafkaWriter(kurl, initialize.KafkaStudentCreated)
+	kurl := fmt.Sprintf("%s:%d", s.cf.Kafka.Host, s.cf.Kafka.Port)
+	kwauc := kafkas.NewKafkaWriter(kurl, events.AuthUserCreated)
+	kwsstd := map[string]*kafka.Writer{
+		events.AuthCreateUser:     kafkas.NewKafkaWriter(kurl, events.AuthCreateUser),
+		string(events.NotiCreate): kafkas.NewKafkaWriter(kurl, string(events.NotiCreate)),
+	}
 
 	// repository
 	authRepo := authRepository.NewAuthRepository(s.pg)
@@ -47,15 +58,18 @@ func (s *Server) MapHandlers() error {
 	ftRepo := ftRepository.NewFacultyRepository(s.pg)
 	alRepo := alRepository.NewAuditLogRepository(s.pg)
 	fpRepo := fpRepository.NewFileProcessorRepository(s.pg)
+	notiRepo := notiRepository.NewNotificationRepository(s.pg)
+	cfRepo := cfRepository.NewConfigRepository(s.pg)
 
 	// usecase
-	authUc := authUsecase.NewAuthUsecase(authRepo, s.lg, kwUserCreated, s.e)
-	stdUc := stdUsecase.NewStudentUsecase(stdRepo, s.lg, kwStudentCreated, s.e)
+	authUc := authUsecase.NewAuthUsecase(authRepo, s.lg, kwauc, s.e)
+	stdUc := stdUsecase.NewStudentUsecase(stdRepo, s.lg, kwsstd, s.e)
 	stuUc := stuUsecase.NewStatusUsecase(stuRepo, s.lg)
 	pgUc := pgUsecase.NewProgramUsecase(pgRepo, s.lg)
 	ftUc := ftUsecase.NewFacultyUsecase(ftRepo, s.lg)
 	alUc := alUsecase.NewAuditLogUsecase(alRepo, s.lg)
 	fpUc := fpUsecase.NewFileProcessorUsecase(fpRepo, s.lg)
+	cfUc := cfUsecase.NewConfigUsecase(cfRepo, s.lg)
 
 	// handler
 	authHandler := authHttp.NewAuthHandlers(authUc, s.lg)
@@ -65,11 +79,17 @@ func (s *Server) MapHandlers() error {
 	ftHandler := ftHttp.NewFacultyHandlers(ftUc, s.lg)
 	alHandler := alHttp.NewAuditLogHandlers(alUc, s.lg)
 	fpHandler := fpHttp.NewFileProcessingHandlers(fpUc, s.lg)
+	cfHandler := cfHttp.NewConfigHandlers(cfUc, s.lg)
 	rlHandler := rlHttp.NewRbacHandlers(s.e)
 
 	// worker
-	s.w.authWorker = authWorker.NewAuthWorker(authUc)
-	s.w.stdWorker = stdWorker.NewStudentWorker(stdUc)
+	s.w.authWorker = authWorker.NewAuthWorker(authUc, s.lg)
+	s.w.stdWorker = stdWorker.NewStudentWorker(stdUc, s.lg)
+	s.w.notiWorker = notiWorker.NewNotificationWorker(notiRepo, s.lg)
+	s.w.auditWorker = alWorker.NewAuditLogWorker(alUc, s.lg)
+
+	// migration
+	migration.SeedStudents(s.pg, stdUc)
 
 	// router group
 	// r.Use(middleware.CasdoorAuthMiddleware())
@@ -82,6 +102,7 @@ func (s *Server) MapHandlers() error {
 	ftGroup := v1.Group("/faculties")
 	alGroup := v1.Group("/auditlogs")
 	fpGroup := v1.Group("/fileprocessor")
+	cfGroup := v1.Group("/config")
 	rlGroup := v1.Group("/rbac")
 
 	// middleware
@@ -95,6 +116,7 @@ func (s *Server) MapHandlers() error {
 	ftHttp.MapFacultyHandlers(ftGroup, ftHandler)
 	alHttp.MapAuditLogHandlers(alGroup, alHandler)
 	fpHttp.MapfileProcessingHandlers(fpGroup, fpHandler)
+	cfHttp.MapconfigHandlers(cfGroup, cfHandler)
 	rlHttp.MaprbacHandlers(rlGroup, rlHandler)
 
 	return nil

cb9c0b0695a1e7d778c4a343cbe4ac815e09da92
Npeka <npkhang22@clc.fitus.edu.vn>
2025-03-10 22:42:43 +0700

    refactor: update pkg kafka

--- pkg/kafka/kafka.go
+++ pkg/kafka/kafka.go
@@ -1,13 +1,24 @@
-package kafkaUtils
+package kafkas
 
 import (
+	"context"
+	"fmt"
+	"log"
 	"net"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/segmentio/kafka-go"
 )
+
+type KafkaReader struct {
+	Topic   string
+	GroupID string
+	Handler func(context.Context, kafka.Message) error
+	MaxIns  int
+}
 
 func NewKafkaWriter(kafkaURL, topic string) *kafka.Writer {
 	return &kafka.Writer{
@@ -64,4 +75,71 @@ func NewKafkaTopic(kafkaURL, topic string) {
 	if err != nil {
 		panic(err.Error())
 	}
+}
+
+func WaitForTopic(kurl, topic string, timeout time.Duration) error {
+	start := time.Now()
+
+	// Connect to Kafka
+	conn, err := kafka.Dial("tcp", kurl)
+	if err != nil {
+		return fmt.Errorf("unable to connect to Kafka: %v", err)
+	}
+	defer conn.Close()
+
+	for {
+		// Get the list of topics from Kafka
+		partitions, err := conn.ReadPartitions()
+		if err != nil {
+			return fmt.Errorf("error reading partitions: %v", err)
+		}
+
+		// Check if the topic exists
+		for _, p := range partitions {
+			if p.Topic == topic {
+				log.Printf("Topic '%s' exists!", topic)
+				return nil
+			}
+		}
+
+		// If timeout is reached, exit the loop
+		if time.Since(start) > timeout {
+			return fmt.Errorf("timeout: topic '%s' was not created after %v", topic, timeout)
+		}
+
+		log.Println("Topic not ready, waiting for 2s...")
+		time.Sleep(2 * time.Second) // Wait before checking again
+	}
+}
+
+func StartKafkaConsumer(kafkaReader *kafka.Reader, handleFunc func(context.Context, kafka.Message) error) {
+	ctx := context.Background()
+	for {
+		msg, err := kafkaReader.ReadMessage(ctx)
+		if err != nil {
+			log.Printf("Error reading message: %v", err)
+			continue
+		}
+
+		if err := handleFunc(ctx, msg); err != nil {
+			log.Printf("Error handling message: %v", err)
+		}
+	}
+}
+
+func StartKafkaConsumers(kurl string, krs []KafkaReader) {
+	var wg sync.WaitGroup
+	for _, kr := range krs {
+		for i := 0; i < kr.MaxIns; i++ {
+			wg.Add(1)
+			krc := kr
+			go func(kr KafkaReader) {
+				defer wg.Done()
+				krnew := NewKafkaReader(kurl, kr.Topic, kr.GroupID)
+				WaitForTopic(kurl, kr.Topic, 5*time.Minute)
+				StartKafkaConsumer(krnew, kr.Handler)
+			}(krc)
+		}
+	}
+	wg.Wait()
 }

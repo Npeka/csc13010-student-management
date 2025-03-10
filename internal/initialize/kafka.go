1ff55c1fca6abb80caa48e3e1f6a058d81308990
Npeka <npkhang22@clc.fitus.edu.vn>
2025-03-10 22:27:17 +0700

    refactor: update init kafka

--- internal/initialize/kafka.go
+++ internal/initialize/kafka.go
@@ -1,51 +1,25 @@
 package initialize
 
 import (
-	"context"
 	"fmt"
-	"log"
 
 	"github.com/csc13010-student-management/config"
-	kafkaUtils "github.com/csc13010-student-management/pkg/kafka"
-	"github.com/segmentio/kafka-go"
+	"github.com/csc13010-student-management/internal/events"
+	kafkas "github.com/csc13010-student-management/pkg/kafka"
 )
 
-const (
-	KafkaAuthUserCreated     = "auth.user.created"
-	KafkaAuthUserUpdated     = "auth.user.updated"
-	KafkaStudentCreated      = "student.created"
-	KafkaStudentUpdated      = "student.updated"
-	KafkaNotificationCreated = "notification.created"
-	KafkaNotificationUpdated = "notification.updated"
-)
+const ()
 
 func NewKafkaTopics(cfg config.KafkaConfig) {
 	kurl := fmt.Sprintf("%s:%d", cfg.Host, cfg.Port)
 	topics := []string{
-		KafkaAuthUserCreated,
-		KafkaAuthUserUpdated,
-		KafkaStudentCreated,
-		KafkaStudentUpdated,
-		KafkaNotificationCreated,
-		KafkaNotificationUpdated,
+		events.AuthUserCreated,
+		events.AuthCreateUser,
+		string(events.NotiStudentStatusChanged),
+		string(events.NotiUserResetPasswordOTP),
 	}
 
 	for _, topic := range topics {
-		kafkaUtils.NewKafkaTopic(kurl, topic)
-	}
-}
-
-func StartKafkaConsumer(kafkaReader *kafka.Reader, handleFunc func(context.Context, kafka.Message) error) {
-	ctx := context.Background()
-	for {
-		msg, err := kafkaReader.ReadMessage(ctx)
-		if err != nil {
-			log.Printf("Error reading message: %v", err)
-			continue
-		}
-
-		if err := handleFunc(ctx, msg); err != nil {
-			log.Printf("Error handling message: %v", err)
-		}
+		kafkas.NewKafkaTopic(kurl, topic)
 	}
 }
